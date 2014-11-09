3f8f1294ac73cfc6a9cb8a0325ca2230e35411d8
Drew Blount <dblount@reed.edu>
2014-11-08 21:40:56 -0800

    working likelihood function in scipy_dace

--- thesis/code/numpy_dace/toy_prob.py
+++ thesis/code/numpy_dace/toy_prob.py
@@ -9,29 +9,33 @@
 
 import matplotlib.pyplot as plt
 ## import my code
-##from dace import *
+from scipy_dace import *
 
 ## X is the vector of input vectors, Y output
-X = [[1.5],[3.5]]
-Y = [[0.25],[0.75]]
+X = [[1.5],[3.5],[2.5],[3.0],[0.5]]
+Y = [[0.25],[0.75],[0.9],[0.8],[0.1]]
 
 ## Plot the evaluated input/outputs
 plt.scatter(X, Y)
 
 ## generate the predictor function:
 ## DACE parameters p and theta (only one of each)
-P = [[1.0]]
-Q = [[1.0]]
-predictor = dace_predictor(X,Y,P,Q)
+P = [1.5]
+Q = [1.0]
+predictor = dace_predictor(X,Y,P,Q,verbose=True)
 
 pred_range = np.arange(0.0, 5.0, 0.01)
-plt.plot(pred_range, predictor(pred_range))
+preds = [ predictor([x]) for x in pred_range ]
+
+plt.plot(pred_range, preds)
 
 
 ## generate the estimated error function:
 
 
 ## Evaluate the likelihood of these Ys given Xs, Ps, Qs:
+lik = conc_likelihood(X, Y, P, Q)
+print("likelihood = %.3f" % lik)
 
 
 
