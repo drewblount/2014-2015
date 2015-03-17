7f75b92f4226374d203e65be44e942aded6adde2
Drew Blount <dblount@reed.edu>
2015-03-16 23:56:09 -0700

    first commit of smbo folder---a refactoring of my thesis code that is also consistent with sphinx documentation

---
+++ thesis/code/smbo/README.md
@@ -0,0 +1,7 @@
+thesis/code/smbo
+===========
+
+
+The ultimate repository of my thesis's code--built first from a refactoring of the numpy_dace folder
+
+Contains a package used for sequential model-based optimization, primarily of the EGO type
---
+++ thesis/code/smbo/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='smbo',
+      version='0.1',
+      description='a sequential model-based optimization framework',
+      url='http://github.com/drewblount/smbo',
+      author='Drew Blount',
+      author_email='dblount@reed.edu',
+      license='MIT',
+      packages=['smbo'],
+      zip_safe=False)
